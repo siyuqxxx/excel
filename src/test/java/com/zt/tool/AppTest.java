3df57723b1a27e624a0bbec40cf63b9cd776d511
qiansy <qiansy@beijingzhangtu.com>
2019-02-01 08:40:58 +0800

    [story][UT] 创建 excel 工具类
    [memo]
    将数据导出到excel的板式代码封装起来
    通过 java8 函数接口，将用户最为关心的填充数据的业务分离出来

--- src/test/java/com/zt/tool/AppTest.java
+++ src/test/java/com/zt/tool/AppTest.java
@@ -1,20 +1,46 @@
 package com.zt.tool;
 
-import static org.junit.Assert.assertTrue;
-
+import jxl.write.Label;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void dataList2excel() {
+        ArrayList<Product> data = new ArrayList<>();
+        data.add(new Product().setName("PC").setDescription("personal computer").setPrice("3999.00"));
+
+        SheetDataContainer<Product> container = new SheetDataContainer<>();
+        container.setSheetName("data list");
+        container.addHead("产品名称").addHead("产品描述").addHead("单价");
+        container.setWriter(((sheet, row, been) -> {
+            sheet.addCell(new Label(0, row, been.getName()));
+            sheet.addCell(new Label(0, row, been.getDescription()));
+            sheet.addCell(new Label(0, row, been.getPrice()));
+        }));
+        container.setData(data);
+
+        List<SheetWriter> writers = new LinkedList<>();
+        writers.add(new SheetWriter<Product>().addData(container));
+
+        File file = new File("C:\\Users\\think\\Desktop\\test.xls");
+        try (FileOutputStream stream = new FileOutputStream(file)) {
+            new ExcelHelper().writeWorkBook(stream, writers);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail();
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 }
---
+++ src/test/java/com/zt/tool/Product.java
@@ -0,0 +1,34 @@
+package com.zt.tool;
+
+public class Product {
+    private String name = "";
+    private String description = "";
+    private String price = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Product setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public Product setPrice(String price) {
+        this.price = price;
+        return this;
+    }
+}

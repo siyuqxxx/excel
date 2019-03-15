54bd3f940846cb7fd0f2fc8235c35cd05e9c7e21
qiansy <qiansy@beijingzhangtu.com>
2019-03-15 18:26:27 +0800

    [story]#3 支持导入
    [memo]

--- src/main/java/com/zt/tool/ExcelHelper.java
+++ src/main/java/com/zt/tool/ExcelHelper.java
@@ -80,6 +80,8 @@ public class ExcelHelper {
 
                 List<String> actualTableHead = Arrays.stream(sheet.getRow(TABLE_HEAD)).map(Cell::getContents).collect(Collectors.toList());
                 List<String> exceptTableHead = ToExcelParser.getHeader(type);
+
+
                 int columns = sheet.getColumns();//得到所有的列
                 for (int row = 1; row < rows; row++) {
                     for (int column = 1; column < columns; column++) {
@@ -111,4 +113,36 @@ public class ExcelHelper {
         }
         return true;
     }
+
+    /**
+     * 首先进行入参检查防止出现空指针异常
+     * 如果两个参数都为空，则返回true
+     * 如果有一项为空，则返回false
+     * 接着对第一个list进行遍历，如果某一项第二个list里面没有，则返回false
+     * 还要再将两个list反过来比较，因为可能一个list是两一个list的子集
+     * 如果成功遍历结束，返回true
+     * @param l0
+     * @param l1
+     * @return
+     */
+    public static <T> boolean isListEqual(List<T> l0, List<T> l1){
+        if (l0 == l1)
+            return true;
+        if (l0 == null && l1 == null)
+            return true;
+        if (l0 == null || l1 == null)
+            return false;
+        if (l0.size() != l1.size())
+            return false;
+        for (Object o : l0) {
+            if (!l1.contains(o))
+                return false;
+        }
+        for (Object o : l1) {
+            if (!l0.contains(o))
+                return false;
+        }
+        return true;
+    }
+
 }
